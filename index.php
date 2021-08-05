4636fcfc9e02a88c339793484a5c201196a08104
StephenLivings <81984263+StephenLivings@users.noreply.github.com>
2021-08-05 20:04:33 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
-include('includes/functions.php')
-?>
 
+include('connlocal.php');
+$allRecords = selectAll();
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -9,13 +11,59 @@ include('includes/functions.php')
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://rawgit.com/outboxcraft/beauter/master/beauter.min.css">
+
+   <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=DM+Sans|Roboto:wght@100">
+
+
     <title>All Linked Resources on The Troubles</title>
+   <?php 
+   //include('theme/header-scripts.php'); 
+   ?>
 </head>
 <body>
-    <h1>All Resources</h1>
+   // <?php include("theme/header.php"); ?>
+    <div class="container fluid">
+    <h1>All Records</h1>
+
+
+
+    <table class="table datatable">
+    <div class="col m12" id="bodytext">
+    <table class="_width100">
+        <thead>
+            <tr>
+
+            <th>ID</th>
+            <th>Resource Title</th>
+</tr>
+</thead>
+
+<tbody>
     <?php
-formatcode(selectAll());
-formatcode(selectSingle(13));
+foreach ($allRecords as $record):
+echo '
+<tr>
+    <td>'.$record['resource_keyid'].' </td>
+    <td>'.$record['resource_title'].' </td>
+    <td class="text-right">
+    <a href="update.php?id='.$record['resource_keyid'].'">Update</a>
+    <a href="delete.php?id='.$record['resource_keyid'].'">Delete</a>
+</tr>
+    ';
+    endforeach;
 ?>
+</tbody>
+</table>
+</div>
+
+
+
+
+
+
 </body>
 </html>
+
