b1f26d73c2284dab6dba0379785ef39c787b35cd
StephenLivings <81984263+StephenLivings@users.noreply.github.com>
2021-08-06 20:32:02 +0100

    Add files via upload

--- trythisapi.php
+++ trythisapi.php
@@ -24,3 +24,49 @@ $allinfo = json_decode($dataset, true);
 </tr>
 </thead>
 <tbody>
+
+<th>name</th>
+        <th>url</th>
+       
+       
+      </tr>
+    </thead>
+    <tbody>
+      
+        
+        <?php
+
+foreach($allinfo as $row){
+
+    //echo "<p>{$row['school_code']}{$row['school_name']}{$row['school_type']}";
+
+
+    //echo "<p>{$row['school_id']}</p>";
+    $row_id= $row['resource_keyid'];
+
+    echo "<tr><div class=''>
+    <th>{$row["resource_title"]}</th>
+                <th><a href='school.php?keyid=$row_id'>{$row["resource_url"]}</a></th>
+             
+        </div></tr> ";
+
+    }
+  
+
+
+
+
+?>
+  </tbody>
+</table>
+
+
+
+ 
+    </header>
+   
+
+
+</body>
+<script src="https://rawgit.com/outboxcraft/beauter/master/beauter.min.js"></script>
+</html>
