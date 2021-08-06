dd9dcd6f136bf8c2e44d37a602f1e664236ed19a
StephenLivings <81984263+StephenLivings@users.noreply.github.com>
2021-08-06 11:06:25 +0100

    Add files via upload

--- connlocal.php
+++ connlocal.php
@@ -1,7 +1,7 @@
 <?php
 //Get Heroku ClearDB connection information
 $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["eu-cdbr-west-01.cleardb.com"];
+$cleardb_server = $cleardb_url["eu-cdbr-west-01.cleardb.comm"];
 $cleardb_username = $cleardb_url["b58663ddf59ae8"];
 $cleardb_password = $cleardb_url["1077328e"];
 $cleardb_db = substr($cleardb_url["heroku_b909e35f617b419"],1);
@@ -13,7 +13,7 @@ $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $c
 if($conn->connect_error){
     echo "not connected".$conn->connect_error;
 }else{
-    echo "connection to DB found.";
+   // echo "connection to DB found.";
 }
 
 ?>
